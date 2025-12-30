fbf02384b40d8f9d3eb5dc3744a56fc449ebaa7c
BenjaminSergio <78599316+BenjaminSergio@users.noreply.github.com>
2025-12-30 18:39:22 -0300

    Update app.py

--- Modelagem/Modelos/app.py
+++ Modelagem/Modelos/app.py
@@ -116,4 +116,4 @@ def predict():
         return jsonify({'message': str(e), 'status': 'error'}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=False)
